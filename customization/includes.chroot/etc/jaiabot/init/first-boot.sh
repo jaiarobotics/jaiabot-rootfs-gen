90e72a3e5dc7a38f5bbbead968681f244f0424d5
Toby Schneider <toby@gobysoft.org>
2022-05-31 20:51:20 +1000

    Fix bugs in first boot script

--- customization/includes.chroot/etc/jaiabot/init/first-boot.sh
+++ customization/includes.chroot/etc/jaiabot/init/first-boot.sh
@@ -15,7 +15,7 @@ fi
 
 echo "###########################################"
 echo "###########################################"
-echo "##### jaiabot first boot init script #####"
+echo "##### jaiabot first boot init script ######"
 echo "###########################################"
 echo "###########################################"
 
@@ -29,13 +29,6 @@ echo "######################################################"
 run_wt_password "Password" "Enter a new password for jaia"
 [ $? -eq 0 ] || exit 1
 echo "jaia:$WT_PASSWORD" | chpasswd
-
-echo "###############################################################"
-echo "## Stress Tests                                              ##" 
-echo "###############################################################"
-
-run_wt_yesno "Hardware checks and stress test" \
-             "Do you want to run the hardware checks and stress test?" && source /etc/jaiabot/init/board-check.sh
 
 echo "###############################################"
 echo "## Resizing data partition to fill disk      ##"
@@ -56,29 +49,23 @@ echo -e "\nResizing filesystem: $JAIABOT_DATA_PARTITION\n"
 # allow jaia user to write logs
 chown -R jaia:jaia /var/log/jaiabot
 
+echo "###############################################################"
+echo "## Stress Tests                                              ##" 
+echo "###############################################################"
+
+run_wt_yesno "Hardware checks and stress test" \
+             "Do you want to run the hardware checks and stress test?" && source /etc/jaiabot/init/board-check.sh
+
 echo "###############################################"
 echo "## Setting up i2c                            ##"
 echo "###############################################"
 
-groupadd i2c
+groupadd -f i2c
 chown :i2c /dev/i2c-1
 chmod g+rw /dev/i2c-1
-usermod -aG i2c ubuntu
+usermod -aG i2c jaia
 udev_entry='KERNEL=="i2c-[0-9]*", GROUP="i2c"'
 grep "$udev_entry" /etc/udev/rules.d/10-local_i2c_group.rules || echo "$udev_entry" >> /etc/udev/rules.d/10-local_i2c_group.rules
-
-echo "###############################################"
-echo "## Setting up swap partition                 ##"
-echo "###############################################"
-
-JAIABOT_SWAPFILE=${JAIABOT_DATA_MOUNTPOINT}/swapfile
-
-fallocate -l 2G $JAIABOT_SWAPFILE
-chmod 600 $JAIABOT_SWAPFILE
-mkswap $JAIABOT_SWAPFILE
-swapon $JAIABOT_SWAPFILE
-fstab_entry="$JAIABOT_SWAPFILE swap swap defaults 0 0"
-grep "$fstab_entry" /etc/fstab || echo "$fstab_entry" >> /etc/fstab
 
 echo "###############################################"
 echo "## Disable getty on /dev/ttyS0               ##"
@@ -97,7 +84,7 @@ echo "###############################################"
 echo "## Install jaiabot-embedded package          ##"
 echo "###############################################"
 
-run_wt_yesno "Do you want to install and configure the jaiabot-embedded Debian package?" && apt install -y /opt/jaiabot-embedded*.deb
+run_wt_yesno "Install jaiabot-embedded package" "\nDo you want to install and configure the jaiabot-embedded Debian package?" && apt install -y /opt/jaiabot-embedded*.deb
 
 echo "###############################################################"
 echo "## Removing first-boot hooks so that this does not run again ##"
@@ -112,5 +99,5 @@ echo "JAIABOT_FIRST_BOOT_DATE=\"`date -u`\"" >> /etc/jaiabot/version
 
 # Finish
 
-run_wt_yesno "First boot provisioning complete\n" \
-             "Do you want to reboot into the complete system?" && reboot
+run_wt_yesno "First boot provisioning complete" \
+             "\nDo you want to reboot into the complete system?" && reboot
